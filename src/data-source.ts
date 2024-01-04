ec8018dc8527bf6cf7bfd41ce134f5980ce59c6b
Vinicius Costa <54358510+ViniciusCosta126@users.noreply.github.com>
2024-01-04 13:38:14 -0300

    Configuracoes iniciais do projeto e criando primeiras entidades e migrations

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,5 +1,5 @@
-import 'dotenv/config'
-import 'reflect-metadata'
+import "dotenv/config";
+import "reflect-metadata";
 import { DataSource } from "typeorm";
 
 const port = process.env.DB_PORT as number | undefined;
@@ -11,4 +11,6 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  entities: [`${__dirname}/**/entities/*.{ts,js}`],
+  migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
 });
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import express from 'express'
-import { AppDataSource } from './data-source'
+import express from "express";
+import { AppDataSource } from "./data-source";
 
 AppDataSource.initialize().then(() => {
-    const app = express()
+  const app = express();
 
-    app.use(express.json())
+  app.use(express.json());
 
-    app.get('/', (req, res) => {
-        return res.json('tudo certo')
-    })
+  app.get("/", (req, res) => {
+    return res.json("tudo certo");
+  });
 
-    return app.listen(process.env.PORT)
-})
+  return app.listen(process.env.PORT);
+});
