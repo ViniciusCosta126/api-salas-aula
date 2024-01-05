315486450de2d4acf637a5c53478d4403d581fd3
Vinicius Costa <54358510+ViniciusCosta126@users.noreply.github.com>
2024-01-05 12:04:34 -0300

    Listando as entidades do banco

--- src/controllers/RoomController.ts
+++ src/controllers/RoomController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { roomRepository } from "../repositories/roomRepository";
 import { videoRepository } from "../repositories/videoRepository";
+import { subjectRepository } from "../repositories/subjectRepository";
 
 export class RoomController {
   async create(req: Request, res: Response) {
@@ -20,6 +21,7 @@ export class RoomController {
       return res.status(500).json({ message: "Internal server error" });
     }
   }
+
   async createVideo(req: Request, res: Response) {
     const { title, url } = req.body;
     const { idRoom } = req.params;
@@ -40,6 +42,55 @@ export class RoomController {
       return res
         .status(201)
         .json({ message: "Criado com sucesso!", video: newVideo });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  }
+
+  async roomSubject(req: Request, res: Response) {
+    const { subject_id } = req.body;
+    const { idRoom } = req.params;
+
+    try {
+      const room = await roomRepository.findOneBy({ id: Number(idRoom) });
+
+      if (!room) {
+        return res.status(404).json({ message: "Aula não encontrada" });
+      }
+
+      const subject = await subjectRepository.findOneBy({
+        id: Number(subject_id),
+      });
+
+      if (!subject) {
+        return res.status(404).json({ message: "Disciplina não encontrada" });
+      }
+
+      const roomUpdate = {
+        ...room,
+        subjects: [subject],
+      };
+
+      await roomRepository.save(roomUpdate);
+
+      return res.status(204).send();
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  }
+
+  async list(req: Request, res: Response) {
+    try {
+      const rooms = await roomRepository.find({
+        relations: {
+          subjects: true,
+          videos: true,
+        },
+      });
+
+      return res.json(rooms);
     } catch (error) {
       console.log(error);
       return res.status(500).json({ message: "Internal server error" });
